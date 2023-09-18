1070a0424a3d21b87c0b8a907ed8a0d137cf3c6a
JacksonWc <1535661801@qq.com>
2023-09-18 11:07:10 +0800

    elasticsearch--使用2种方式自行开发搜索功能-第1种是使用JPA风格定义抽象方法；第2种是完全自定义抽象方法（不需要使用JPA风格），然后，在方法上添加@Query注解来配置搜索的JSON字符串（搜索的JSON中的query的属性值）

--- tmall-admin/tmall-admin-mall/src/main/java/cn/tedu/tmall/admin/mall/dao/search/GoodsElasticsearchRepository.java
+++ tmall-admin/tmall-admin-mall/src/main/java/cn/tedu/tmall/admin/mall/dao/search/GoodsElasticsearchRepository.java
@@ -1,10 +1,39 @@
 package cn.tedu.tmall.admin.mall.dao.search;
 
-import cn.tedu.tmall.common.pojo.po.GoodsSearchPO;
+
+import cn.tedu.tmall.common.po.GoodsSearchPO;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GoodsElasticsearchRepository
-        extends CrudRepository<GoodsSearchPO, Long> {
+        extends ElasticsearchRepository<GoodsSearchPO, Long> {
+
+    List<GoodsSearchPO> findByTitle(String x);
+
+
+
+    @Query("{\n" +
+            "    \"bool\": {\n" +
+            "      \"should\": [\n" +
+            "        {\n" +
+            "          \"match\": {\n" +
+            "            \"title\": \"?0\"\n" +
+            "          }\n" +
+            "        },\n" +
+            "        {\n" +
+            "          \"match\": {\n" +
+            "            \"brief\": \"?0\"\n" +
+            "          }\n" +
+            "        }\n" +
+            "      ]\n" +
+            "    }\n" +
+            "  }")
+        // findByTitleOrBrief(String title, String brief);
+    List<GoodsSearchPO> test(String x);
+
 }
--- tmall-admin/tmall-admin-mall/src/test/java/cn/tedu/tmall/admin/mall/dao/search/GoodsElasticsearchRepositoryTests.java
+++ tmall-admin/tmall-admin-mall/src/test/java/cn/tedu/tmall/admin/mall/dao/search/GoodsElasticsearchRepositoryTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class GoodsElasticsearchRepositoryTests {
 
@@ -19,6 +21,18 @@ public class GoodsElasticsearchRepositoryTests {
         goodsSearchPO.setTitle("2023年明前特级信阳毛尖绿茶 250克（125g*2罐）");
 
         repository.save(goodsSearchPO);
+    }
+
+
+
+    @Test
+    void test() {
+        String str = "礼盒";
+        List<GoodsSearchPO> list = repository.test(str);
+        System.out.println("查询结果中的数据量：" + list.size());
+        for (GoodsSearchPO goodsSearchPO : list) {
+            System.out.println(goodsSearchPO);
+        }
     }
 
 }
